21321dd7893f1eb7862353de7dfba108c39d64d5
I. Fritsch <ines.fritsch@gmail.com>
2022-07-19 23:47:23 +0200

    add check to catch img error on drawObject

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -61,7 +61,13 @@ class DrawableObject {
     }
 
     drawObject(ctx){
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        try {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        }
+        catch(err) {
+            console.warn('Error loading image', err);
+            console.log(' Could not load image ', this.img.src);
+        }
     }
 
     toggleHitboxes() {
