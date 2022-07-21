9155c7b0889f40ba29175b6db71eefe1a2fb0540
I. Fritsch <ines.fritsch@gmail.com>
2022-07-21 17:54:45 +0200

    [TEST:] maybe remove class

--- models/collectible-object.class.js
+++ models/collectible-object.class.js
@@ -2,16 +2,16 @@ class CollectibleObject extends MovableObject {
     
     // TODO: REMOVE Class?!
 
-    animationFPS = 2;
-    animationFrameInterval = 1000 / this.animationFPS;
-    animationFrameTimer = 0;
+    // animationFPS = 2;
+    // animationFrameInterval = 1000 / this.animationFPS;
+    // animationFrameTimer = 0;
 
-    checkAnimationFrameTime(deltaTime) { // here instead of in bottle and coin extra?
-        if (this.animationFrameTimer > this.animationFrameInterval) {
-            this.pulse(6);
-            this.animationFrameTimer = 0;
-        } else {
-            this.animationFrameTimer += deltaTime;
-        }
-    }
+    // checkAnimationFrameTime(deltaTime) { // here instead of in bottle and coin extra?
+    //     if (this.animationFrameTimer > this.animationFrameInterval) {
+    //         this.pulse(6);
+    //         this.animationFrameTimer = 0;
+    //     } else {
+    //         this.animationFrameTimer += deltaTime;
+    //     }
+    // }
 }
